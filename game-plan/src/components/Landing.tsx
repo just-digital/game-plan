3554ab24c767ffa8a6bdf0f1f77cb7c47f15154f
Kevin Sparks <kevin@just-digital.net>
2023-11-24 12:09:19 +1300

    aider: Create and export a `Landing` component.
    # Aider chat conversation:
    
    USER: Create and export a `Landing` component. The purpose of this page is to display a welcome message with a list menu of options after Auth0 authentication.
    
    ASSISTANT: To make this change we need to modify `game-plan/src/components/Landing.tsx`:
    
    1. Create a functional component named `Landing`.
    2. Display a welcome message.
    3. Export the `Landing` component.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    game-plan/src/components/Landing.tsx
    ```tsx
    <<<<<<< SEARCH
    =======
    import React from 'react';
    
    const Landing = () => {
      return (
        <div>
          <h1>Welcome!</h1>
          <p>Please select an option from the menu.</p>
        </div>
      );
    };
    
    export default Landing;
    >>>>>>> REPLACE
    ```
    
    Please note that this is a basic implementation of the `Landing` component. You may want to customize it according to your needs, such as adding the list menu of options and integrating with Auth0 authentication.

---
+++ game-plan/src/components/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>Please select an option from the menu.</p>
+    </div>
+  );
+};
+
+export default Landing;
